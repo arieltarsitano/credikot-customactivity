5be05f40d2d2f9d447d61a3fde0e0bed6230354b
Ariel Tarsitano <ariel089@MacBook-Pro-de-Ariel.local>
2021-09-24 00:21:04 -0500

    Version inicial

--- routes/activity.js
+++ routes/activity.js
@@ -97,7 +97,7 @@ exports.execute = function (req, res) {
                 'headers': {
                     'Content-Type': 'application/json'
                 },
-                body: util.inspect(decoded)
+                body: util.inspect(decodedArgs)
 
             };
             request(options, function (error, response) {
