874cfbe425a88e011d03d113cc180a156f827909
Ignacio Nogueira <ignacio.nogueira@virtualdreams.io>
2021-10-22 17:14:24 -0300

    Testing payload 20

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -76,7 +76,7 @@ define([
 
 
         //console.log(inArguments[0].Mensaje);
-        document.getElementById('content').value = (JSON.stringify(inArguments[0].Mensaje)).replace('"', '');
+        document.getElementById('content').value = (JSON.stringify(inArguments[0].Mensaje)).replaceAll('"', '');
         console.log('mensajeGuardadoacá')
         console.log(document.getElementById('content').value);
         //console.log(inArguments);
