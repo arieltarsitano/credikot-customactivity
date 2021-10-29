4410db18ff9dc2bea3f9e8ceab37e6403d009586
Ignacio Nogueira <ignacio.nogueira@virtualdreams.io>
2021-10-29 14:23:59 -0300

    return contenidos3

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -76,8 +76,8 @@ define([
 
         var tamJson = (JSON.stringify(inArguments[0].Mensaje)).length;
         var contenidoMensaje = (JSON.stringify(inArguments[0].Mensaje)).substring(1, tamJson - 1);
-        var nombrePersona = (JSON.stringify(inArguments[0].Nombre)).substring(1, tamJson - 1);
-        var monto = (JSON.stringify(inArguments[0].Monto)).substring(1, tamJson - 1);
+        //var nombrePersona = (JSON.stringify(inArguments[0].Nombre)).substring(1, tamJson - 1);
+        //var monto = (JSON.stringify(inArguments[0].Monto)).substring(1, tamJson - 1);
 
         contenidoMensaje = contenidoMensaje.replaceAll('\n', '\n');
         contenidoMensaje = contenidoMensaje.replaceAll('\\', '');
