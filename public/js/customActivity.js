cd51fea410a4783d7d2af0400c7066018f099db0
Ignacio Nogueira <ignacio.nogueira@virtualdreams.io>
2021-11-04 12:14:03 -0300

    testing nombre y monto7

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -140,8 +140,8 @@ define([
 
         payload['metaData'].isConfigured = true;
         payload['arguments'].execute.inArguments[0].Mensaje = document.getElementById('content').value;
-        payload['arguments'].execute.inArguments[0].Nombre = document.getElementById('Contact.Attribute.Mora.Nombre').value;
-        payload['arguments'].execute.inArguments[0].Monto = document.getElementById('Contact.Attribute.Mora.Monto').value;
+        payload['arguments'].execute.inArguments[0].Nombre = document.getElementById('content2').value;
+        payload['arguments'].execute.inArguments[0].Monto = document.getElementById('content2').value;
 
         console.log('se mete a save2222222');
         console.log(payload.arguments.execute.inArguments);
