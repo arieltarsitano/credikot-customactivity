6fa0c21a3720451572679eb572086deb9c212545
Ignacio Nogueira <ignacio.nogueira@virtualdreams.io>
2021-11-08 10:24:30 -0300

    testing funciona

--- routes/activity.js
+++ routes/activity.js
@@ -65,7 +65,9 @@ exports.save = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
     logData(req);
-    res.send(200, 'Save');
+    //res.send(200, 'Save');
+    res.status(200).send('Save');
+
 };
 
 /*
