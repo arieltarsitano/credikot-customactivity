fc4de4f2306f41745dfa252c7bb5e6821d4e6a80
Ignacio Nogueira <ignacio.nogueira@virtualdreams.io>
2021-12-13 14:18:56 -0300

    testing7

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -287,6 +287,7 @@ define([
             payload['arguments'].execute.inArguments[0].nroWPP = "{{Contact.Attribute.30092021_Journey_Mora.nroWPP}}"
             payload['arguments'].execute.inArguments[0].linkWPP = "{{Contact.Attribute.30092021_Journey_Mora.linkWPP}}"
             payload['arguments'].execute.inArguments[0].Telefono = "{{Contact.Attribute.30092021_Journey_Mora.Telefono}}"
+            payload['arguments'].execute.inArguments[0].Feriados = sinBlancos(document.getElementById('content2').value);
             payload['arguments'].execute.inArguments[0].Boton = "Mora";
             console.log('Entró a guardar j1');
 
@@ -297,6 +298,7 @@ define([
             payload['arguments'].execute.inArguments[0].nroWPP = "{{Contact.Attribute.CredikotJourney2.nroWPP}}"
             payload['arguments'].execute.inArguments[0].linkWPP = "{{Contact.Attribute.CredikotJourney2.linkWPP}}"
             payload['arguments'].execute.inArguments[0].Telefono = "{{Contact.Attribute.CredikotJourney2.Telefono}}"
+            payload['arguments'].execute.inArguments[0].Feriados = sinBlancos(document.getElementById('content2').value);
             payload['arguments'].execute.inArguments[0].Boton = "Venta";
             console.log('Entró a guardar j2');
         }
